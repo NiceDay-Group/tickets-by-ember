c51b5b642a6811d71c94c7f9310e36d103b312cd
Aliaksei Krasikau <a.krasikau@gmail.com>
2015-11-04 21:54:24 +0300

    Add ticket route

--- app/router.js
+++ app/router.js
@@ -8,6 +8,7 @@ var Router = Ember.Router.extend({
 Router.map(function() {
   this.route('index', { path: '/' }, function() {
     this.route('index', { path: '/'});
+    this.route('ticket');
     this.route('tickets');
   });
   this.route('signin');
