f95109a0b0f0537fcfb4c8eb90a29ee7613cdeb3
Aliaksei Krasikau <a.krasikau@gmail.com>
2015-11-04 22:58:51 +0300

    Remove bus ticket id from model

--- app/models/ticket.js
+++ app/models/ticket.js
@@ -2,7 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   userId: DS.attr('string'),
-  busTicketId: DS.attr('number'),
   busId: DS.attr('number'),
   dateCreated: DS.attr('date'),
 });
